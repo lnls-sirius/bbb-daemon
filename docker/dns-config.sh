d4349b06b31fc6fb4e3609b7d206958a1b59ca5d
carneirofc <claudiofcarneiro@hotmail.com>
2018-05-24 11:59:44 -0300

    docker cfgs

--- docker/dns-config.sh
+++ docker/dns-config.sh
@@ -1,11 +1,20 @@
-#echo "nameserver $SERVER_IP_ADDR" > /etc/resolv.conf
+#!/usr/bin/env bash
 
-echo "nameserver $DNS_SERVER_1" > /etc/resolv.conf
-echo "nameserver $DNS_SERVER_2" >> /etc/resolv.conf
+ping 10.0.6.70
 
-echo "search abtlus.org.br" >> /etc/resolv.conf
+if [ "$DNS_USE_DNS_PVS" == "YES" ]; then
+	echo "nameserver $DNS_SERVER_1" > /etc/resolv.conf
+	echo "nameserver $DNS_SERVER_2" >> /etc/resolv.conf
 
+	echo "search abtlus.org.br" >> /etc/resolv.conf
 
-cat /etc/resolv.conf
+	cat /etc/resolv.conf
+	echo "DNS configured !"
+else
+	echo "/etc/resolv.conf:"
+	cat /etc/resolv.conf
+	echo "DNS Default !"
+fi
+
 echo "Ping www.google.com"
 ping www.google.com
