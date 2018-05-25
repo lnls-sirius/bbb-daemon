1b628db617169cf515cc67990706d11f780e3b2a
carneirofc <claudiofcarneiro@hotmail.com>
2018-05-25 10:23:53 -0300

    Added daemon related ports do the Dockerfile.

--- docker/docker-run-container.sh
+++ docker/docker-run-container.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker run -d -p 6379:6379 -p 6789:6789 --name bbb-serv-cont lnlscon/bbb-daemon-container:bbb-server
+#docker run -d -p 6379:6379 -p 6789:6789 --name bbb-serv-cont lnlscon/bbb-daemon-container:bbb-server
+docker run -d  -p 6789:6789 --name bbb-serv-cont lnlscon/bbb-daemon-container:bbb-server
+
