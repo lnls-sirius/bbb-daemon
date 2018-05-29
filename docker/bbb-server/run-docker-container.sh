49733027aeab6ea7a079a3b3a5d7e632d44a790e
carneirofc <claudiofcarneiro@hotmail.com>
2018-05-29 10:44:12 -0300

    Docker configurations.

--- docker/bbb-server/run-docker-container.sh
+++ docker/bbb-server/run-docker-container.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -d  -p 6789:6789 -p 9876:9876 --name bbb-serv-cont lnlscon/bbb-daemon-container:bbb-server
+docker run -d  -p 6789:6789 -p 9876:9876 --name bbb-serv-cont lnlscon/bbb-daemon-container:latest
 
