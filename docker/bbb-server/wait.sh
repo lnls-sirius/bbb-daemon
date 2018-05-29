e446285a3852d91f6673d270bc1d684893a17fd1
carneirofc <claudiofcarneiro@hotmail.com>
2018-05-29 15:36:01 -0300

    Swarm update

--- docker/bbb-server/wait.sh
+++ docker/bbb-server/wait.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd /root/wait-for-it/
-./wait-for-it.sh $REDIS_SERVER_IP:$REDIS_SERVER_PORT
+./wait-for-it.sh -t 0 $REDIS_SERVER_IP:$REDIS_SERVER_PORT
 echo "Redis is UP!"
 cd /root/bbb-daemon/
 git pull
