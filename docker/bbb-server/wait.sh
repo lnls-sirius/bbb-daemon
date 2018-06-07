5176aa1771ec008cd29d01611c7b79b3975ef478
carneirofc <claudiofcarneiro@hotmail.com>
2018-06-07 15:51:33 -0300

    Docker - Update for webserver

--- docker/bbb-server/wait.sh
+++ docker/bbb-server/wait.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 cd /root/wait-for-it/
+echo "Wait for Redis ..."
+echo $REDIS_SERVER_IP
+echo $REDIS_SERVER_PORT
+echo "..."
 ./wait-for-it.sh -t 0 $REDIS_SERVER_IP:$REDIS_SERVER_PORT
 echo "Redis is UP!"
 cd /root/bbb-daemon/
