d3120bb7b835e18fe2104d0be3cdcaeaaa47e8e2
carneirofc <claudiofcarneiro@hotmail.com>
2018-06-07 16:00:42 -0300

    fix env name

--- daemon/run.sh
+++ daemon/run.sh
@@ -2,7 +2,7 @@
 export PYTHONPATH="$(dirname $PWD)"
 
 export SERVER_APP_IP=10.0.6.44
-export BBB_PING_PORT=9876
+export BBB_UDP_PORT=9876
 export BBB_TCP_PORT=9877
 
-python3 /root/bbb-daemon/daemon/daemon.py $SERVER_APP_IP $BBB_PING_PORT $BBB_TCP_PORT
+python3 /root/bbb-daemon/daemon/daemon.py $SERVER_APP_IP $BBB_UDP_PORT $BBB_TCP_PORT
