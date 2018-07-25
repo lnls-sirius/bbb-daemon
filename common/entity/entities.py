50ac52b352ca697c435ec5d96b8dde5dd8e640a0
Gustavo Ciotto <gustavociotto@gmail.com>
2018-07-25 09:46:28 -0300

    commentaries and changes to follow PEP in commom libraries

--- common/entity/entities.py
+++ common/entity/entities.py
@@ -1,69 +1,61 @@
-""" This module contains all entity classes of the project. """
+"""
+This module contains all entity classes of the project.
+"""
+
 import ast
+import ipaddress
 
 
-class BaseRedisEntity():
-    key_prefix = 'prefix_'
-    key_prefix_len = len(key_prefix)
+class Command:
+    """
+    A simple class to wrap command codes.
+    """
 
-    def toSet(self):
-        """
-            Object to set used for the redis serialization
-        :return: key, content
-        """
-        pass
-
-    def fromSet(self, str_dic):
-        """
-            Get the values from a dictionary originated from the deserialization of a redis query
-        :param str_dic:
-        :return:
-        """
-        pass
-
-    def get_key(self):
-        """
-            Gets the key used on redis.
-        :return: id for redis.
-        """
-        pass
-
-    @staticmethod
-    def get_name_from_key(key: str):
-        if len(key) <= Node.key_prefix_len:
-            return ''
-        return key[Node.key_prefix_len:]
+    PING, REBOOT, EXIT, END, TYPE, GET_TYPES, APPEND_TYPE, REMOVE_TYPE, NODE, GET_REG_NODES_SECTOR, \
+        GET_REG_NODE_BY_IP, GET_UNREG_NODES_SECTOR, APPEND_NODE, REMOVE_NODE, SWITCH = range(15)
 
 
-class Sector():
-    SECTORS = ["Conectividade"] + [str(i) for i in range(1, 21)] + ["LINAC", "RF", "Fontes"]
+class Sector:
+    """
+    A static class providing helper functions to manage sectors.
+    """
+    SECTORS = [str(i) for i in range(1, 21)] + ["Conectividade", "LINAC", "RF", "Fontes"]
+    SUBNETS = [ipaddress.ip_network('10.128.{}.0/24'.format(i * 10)) for i in range(1, 21)] + \
+                [ipaddress.ip_network('10.128.0.0/24'), ipaddress.ip_network('10.128.1.0/24'),
+                 ipaddress.ip_network('10.128.25.0/24'), ipaddress.ip_network('10.128.75.0/24')]
 
     @staticmethod
     def sectors():
         return Sector.SECTORS
 
+    @staticmethod
+    def get_sector_by_ip_address(ip_address=None):
+        """
+        @todo use ipaddress module to save subnet addresses of each sector.
+        :param ip_address: the IP address of a host.
+        :return: the sector that contains the given IP address.
+        """
 
-class Command():
-    PING, REBOOT, EXIT, END, TYPE, GET_TYPES, APPEND_TYPE, REMOVE_TYPE, NODE, GET_REG_NODES_SECTOR, GET_UNREG_NODES_SECTOR, APPEND_NODE, REMOVE_NODE, SWITCH = range(
-        14)
+        for idx, subnet in enumerate(Sector.SUBNETS):
+            if ip_address in subnet.hosts():
+                return Sector.SECTORS[idx]
+
+        return -1
 
 
-class NodeState():
-    """ Valid states for any host in the Controls Group network. """
+class NodeState:
+    """
+    Valid states for any host in the Controls Group network.
+    """
 
     DISCONNECTED, MISCONFIGURED, CONNECTED, REBOOTING = range(4)
 
-    MISCONFIGURED_COLOR_STACK = [(255, 255, 153), (253, 255, 0), (204, 204, 255), (220, 220, 220)]
-    MISCONFIGURED_INDEX = 1
-
     @staticmethod
-    def useColor():
-        NodeState.MISCONFIGURED_INDEX = (NodeState.MISCONFIGURED_INDEX + 1) % len(NodeState.MISCONFIGURED_COLOR_STACK)
-        return NodeState.MISCONFIGURED_COLOR_STACK[NodeState.MISCONFIGURED_INDEX]
-
-    # String representation of a state
-    @staticmethod
-    def toString(state):
+    def to_string(state):
+        """
+        :param state: the state to be converted to string.
+        :return: string representation of a state.
+        """
 
         if state == NodeState.DISCONNECTED:
             return "Disconnected"
@@ -80,38 +72,103 @@ class NodeState():
         return "Unknown state"
 
 
+class BaseRedisEntity:
+    """
+    A base abstract class for all entries stored in the Redis db. Classes implementing it should provide at least
+    three methods: to_set, from_set and get_key.
+    """
+
+    key_prefix = 'prefix_'
+    key_prefix_len = len(key_prefix)
+
+    def to_set(self):
+        """
+        Returns a set representation of the object.
+        """
+        pass
+
+    def from_set(self, str_dic):
+        """
+        Returns a BaseRedisEntity object from a string representation of dictionary queried from the Redis db.
+        :param str_dic: python's string representation of the dictionary.
+        """
+        pass
+
+    def get_key(self):
+        """
+        Gets the key used on redis.
+        :return: id for redis.
+        """
+        pass
+
+    @staticmethod
+    def get_name_from_key(key: str):
+        """
+        Removes the prefix from the key.
+        :param key: A Redis key.
+        :return: The key without prefix.
+        """
+        if len(key) <= BaseRedisEntity.key_prefix_len:
+            return ''
+
+        return key[BaseRedisEntity.key_prefix_len:]
+
+
 class Type(BaseRedisEntity):
-    """ This class provides a wrapper for host types. """
+    """
+    This class provides a wrapper for host types.
+    """
+
     key_prefix = 'type_'
     key_prefix_len = len(key_prefix)
 
-    def __init__(self, name="generic", repoUrl="A generic URL.", color=(255, 255, 255),
+    def __init__(self, name="generic", repo_url="A generic URL.", color=(255, 255, 255),
                  description="A generic host.", sha: str = ""):
+        """
+        Initializes a type instance.
+        :param name: a type's name.
+        :param repo_url: the repository address with all files needed to execute its function.
+        :param color: A color to be used to represent the type in visual clients.
+        :param description: A brief description of the type
+        :param sha: A way to provide error detection.
+        """
         self.name = name
         self.color = color
-        self.repoUrl = repoUrl
+        self.repoUrl = repo_url
         self.description = description
         self.sha = sha
 
     @staticmethod
     def get_name_from_key(key: str):
+        """
+        Returns the prefix from a type's name.
+        :param key: a type's id key.
+        :return: a type's name without the prefix
+        """
         if len(key) <= Type.key_prefix_len:
             return ''
+
         return key[Type.key_prefix_len:]
 
-    def toSet(self):
-        key = self.get_key()
+    def to_set(self):
+        """
+        Returns the object representation that will be saved on the Redis db.
+        :return: string representation of dict representing a type
+        """
         content = str({k: vars(self)[k] for k in ("description", "color", "repoUrl", "sha")})
-        return key, content
+        return self.get_key(), content
 
     def get_key(self):
+        """
+        Returns the name concatenated with the Type prefix. To be used to save instances into a Redis db.
+        :return: the name concatenated with the Type prefix.
+        """
         return Type.key_prefix + self.name
 
-    def fromSet(self, str_dic):
+    def from_set(self, str_dic):
         """
-            Load the values from a redis set.
-        :param str_dic:
-        :return:
+        Load values from a redis set.
+        :param str_dic: python's string representation of the dictionary.
         """
         if str_dic is None:
             return
@@ -127,6 +184,11 @@ class Type(BaseRedisEntity):
             self.sha = dic_obj.get("sha", '')
 
     def __eq__(self, other):
+        """
+        A way to compare two types.
+        :param other: other type instance.
+        :return: True if the other's name is equal to the self's name, False otherwise.
+        """
         if other is None:
             return False
 
@@ -138,33 +200,45 @@ class Type(BaseRedisEntity):
     def __str__(self):
         return str(self.__dict__)
 
+
 class Node(BaseRedisEntity):
     """
-        This class represents a Controls group's host.
-        Each host has a symbolic name, a valid IP address, a type and the sector where it is located.
+    This class represents a Controls group's host. Each host has a symbolic name, a valid IP address, a type
+    and the sector where it is located.
     """
     key_prefix = 'node_'
     key_prefix_len = len(key_prefix)
 
-    def __init__(self, name="r0n0", ip="10.128.0.0", state=NodeState.DISCONNECTED, typeNode: Type = None, sector=1,
-                 counter=0,
-                 pvPrefix=[], rcLocalPath=''):
+    REBOOT_COUNTER_PERIOD = -90
+
+    def __init__(self, name="r0n0", ip="10.128.0.0", state=NodeState.DISCONNECTED, type_node: Type = None, sector=1,
+                 counter=0, pv_prefixes=[], rc_local_path=''):
+        """
+        Initializes a node instance
+        :param name: a node's name
+        :param ip: string representation of a node's ip address
+        :param state: current node's state
+        :param type_node: current node's type
+        :param sector: current node's sector
+        :param counter: heart beat count
+        :param pv_prefixes: deprecated - a BBB will not provide PVs
+        :param rc_local_path: rc.local location in the project
+        """
 
         self.name = name
         self.ipAddress = ip
         self.state = state
-        self.state_string = NodeState.toString(state)
-        self.misconfiguredColor = None
-        self.type = typeNode
+        self.state_string = NodeState.to_string(state)
+        self.type = type_node
         self.sector = sector
-        self.pvPrefix = pvPrefix
+        self.pvPrefix = pv_prefixes
         self.counter = counter
-        self.rcLocalPath = rcLocalPath
+        self.rcLocalPath = rc_local_path
 
     @staticmethod
     def get_prefix_string(pref: []):
         """
-            Array to String !
+        Array to String !
         :return:
         """
         pref_str = ''
@@ -195,32 +269,41 @@ class Node(BaseRedisEntity):
 
     @staticmethod
     def get_name_from_key(key: str):
+        """
+        Removes prefix from key and returns the name.
+        :param key:
+        :return: the node's name without prefix.
+        """
         if len(key) <= Node.key_prefix_len:
             return ''
         return key[Node.key_prefix_len:]
 
-    def set_prefix(self):
-        pass
+    def to_set(self):
+        """
+        Returns a node's dictionary representation.
+        :return: node's key with prefix and the node's dictionary representation.
+        """
 
-    def toSet(self):
-        key = self.get_key()
-        nodeInfo = {"ipAddress": self.ipAddress, "sector": self.sector, "prefix": self.pvPrefix,
-                    "rcLocalPath": self.rcLocalPath}
+        node_info = {"ipAddress": self.ipAddress, "sector": self.sector, "prefix": self.pvPrefix,
+                     "rcLocalPath": self.rcLocalPath}
+
         if self.type is None:
-            nodeInfo["type"] = None
+            node_info["type"] = None
         else:
-            nodeInfo['type'] = self.type.name
-        content = str(nodeInfo)
-        return key, content
+            node_info['type'] = self.type.name
+
+        return self.get_key(), str(node_info)
 
     def get_key(self):
+        """
+        :return: returns the node's key with prefix
+        """
         return (Node.key_prefix + self.name).replace(' ', '')
 
-    def fromSet(self, str_dic):
+    def from_set(self, str_dic):
         """
-            Load the values from a redis set.
-        :param str_dic:
-        :return:
+        Load the values from a redis set.
+        :param str_dic: python's string representation of the dictionary.
         """
         if str_dic is None:
             return
@@ -235,23 +318,36 @@ class Node(BaseRedisEntity):
             self.pvPrefix = dic_obj.get("prefix", [])
             self.sector = dic_obj.get("sector", self.sector)
 
-    # Change the current state of the object. Refer to the Control_Node_State class
-    def changeState(self, state):
+    def update_state(self, state):
+        """
+        Updates the current node's state.
+        :param state: new node state.
+        :return:
+        """
         self.state = state
-        self.state_string = NodeState.toString(state)
+        self.state_string = NodeState.to_string(state)
 
-    # Returns True if the state is Control_Node_State.CONNECTED
-    def isConnected(self):
-        return (self.state != NodeState.DISCONNECTED)
+    def is_connected(self):
+        """
+        :return: True if the state is NodeState.CONNECTED. False, otherwise.
+        """
+        return self.state != NodeState.DISCONNECTED
 
     def __eq__(self, other):
+        """
+        Overrides == operator. Compares two Node objects.
+        :param other: a other node instance.
+        :return: True if the other instance has the same name and IP address.
+        """
         if other is None:
             return False
 
         return self.name == other.name or self.ipAddress == other.ipAddress
 
-    # Returns the string representation of the object
     def __str__(self):
+        """
+        :return: the string representation of the object
+        """
         return "Name: %s, IP Address: %s, Current state: %s" % (
-            self.name, self.ipAddress, NodeState.toString(self.state))
+            self.name, self.ipAddress, NodeState.to_string(self.state))
 
--- common/network/utils.py
+++ common/network/utils.py
@@ -5,148 +5,186 @@ import struct
 import socket
 import subprocess
 
-re_ipv4 = r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"
-re_mask_ipv4 = r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"
 
-
-def checksum(data_str: str):
-    packet = data_str.strip().encode('utf-8')
-    total = 0
-    # Add up 16-bit words
-    num_words = len(packet) // 2
-    for chunk in struct.unpack("!%sH" % num_words, packet[0:num_words * 2]):
-        total += chunk
-
-    # Add any left over byte
-    if len(packet) % 2:
-        total += ord(chr(packet[-1])) << 8
-
-    # Fold 32-bits into 16-bits
-    total = (total >> 16) + (total & 0xffff)
-    total += total >> 16
-    return (~total + 0x10000 & 0xffff)
-
-
-def get_ip_address(ifname: str):
-    ifname = ifname.encode('utf-8')
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
-
-
-def changeIp(desired_ip: str = None, interface_name: str = 'eth0', net_mask: str = '255.255.255.0',
-             gateway: str = None):
+class NetUtils:
     """
-    @todo: now that i know the service i can do whatever i want !
-    Using connmanclt to change ip address
-    :param desired_ip:
-    :return: (True or False), message
+    A static class to send data through a given connection.
     """
-    if not desired_ip:
-        return False, 'Desired ip not set !'
-    service = getService(interface_name=interface_name)
-    if not service:
-        return False, 'Service could\'t be found'
-
-    # @todo make the magic happen
-    # @todo make two different patterns for mask and ip. Limit them with the correct values.
-    if not re.match(re_mask_ipv4, net_mask):
-        return False, 'The net_mask={} doesn\'t match the mask pattern.'.format(net_mask)
-    if not re.match(re_ipv4, desired_ip):
-        return False, 'The desiredIp={} doesn\'t match the ipv4 pattern.'.format(desired_ip)
-    if gateway:
-        if not re.match(re_ipv4, gateway):
-            return False, 'The gateway={} doesn\'t match the ipv4 pattern.'.format(gateway)
-    else:
-        g_aux = desired_ip.split('.')
-        if len(g_aux) == 4:
-            g_aux[3] = '1'
-            gateway = ''
-            for g in g_aux:
-                gateway += g + '.'
-            gateway = gateway[:-1]
-
-    res = subprocess.check_output(
-        ['connmanctl config {} ipv4 manual {} {} {}'.format(service, desired_ip, net_mask, gateway)], shell=True)
-    return True, 'Successfully modified the ipv4 address. {}'.format(res)
-
-
-def getService(interface_name: str = 'eth0'):
-    """
-    Return the service name from an interface.
-    @fixme: services with spaces on their names won't be detected !
-    :return: The service name or None !
-    """
-    res = subprocess.check_output(['connmanctl services'], stderr=subprocess.STDOUT, shell=True).decode('utf-8')
-    res = res.split()
-    for s in res:
-        if s.startswith('ethernet_'):
-            res = subprocess.check_output(['connmanctl services --properties ' + s], stderr=subprocess.STDOUT,
-                                          shell=True).decode(
-                'utf-8')
-            print(res)
-            res = res.split('\n')
-            for p in res:
-                if p.strip().startswith('Ethernet'):
-                    print('Found it!' + p)
-                    data = p.split('[')[1].strip()[:-1].split(',')
-                    for d_info in data:
-                        d_info = d_info.strip()
-                        if d_info.startswith('Interface'):
-                            print(d_info)
-                            if d_info == 'Interface={}'.format(interface_name):
-                                print('The service related to {} is {}'.format(interface_name, s))
-                                return s
-                    print(data)
-    return None
-
-
-def verify_msg(data: str):
-    """
-        This method must be update as the data format sent by the bbb is modified
-        BBB sends this: {chk} | {cmd} | {name} | {type} | {ipAddr} | {sha}
-    :param data:
-    :return: Array containing the data, [] if error.
-    """
-    splt = data.split("|")
-    if len(splt) != 6:
-        # Error !
-        return []
-    message = data[(len(splt[0]) + 1):]
-    res = checksum(message)
-    if int(res) == int(splt[0]):
-        return splt
-    else:
-        return []
-
-
-class NetUtils():
 
     @staticmethod
-    def sendCommand(connection, command):
-        return connection.send(struct.pack("!i", command))
+    def send_command(connection, command):
+        """
+        Sends a command through the connection.
+        :param connection: the connection to be used to send the integer.
+        :param command: A 4-byte integer representing a command.
+        """
+        connection.send(struct.pack("!i", command))
 
     @staticmethod
-    def recvCommand(connection):
+    def recv_command(connection):
+        """
+        Receives a single 4-byte integer.
+        :param connection: the connection to be used to receive the integer.
+        :return: the integer representing a command.
+        """
         return struct.unpack("!i", connection.recv(4))[0]
 
     @staticmethod
-    def sendData(connection, data):
+    def send_data(connection, data):
+        """
+        Sends bytes through the connection. First, its sends how many bytes the data is composed of.
+        :param connection: the connection to be used to send the object.
+        :param data: the data to bem sent.
+        """
         connection.send(struct.pack("!i", len(data)))
         connection.send(data)
 
     @staticmethod
-    def recvData(connection):
-        dataSize = struct.unpack("!i", connection.recv(4))[0]
-        return connection.recv(dataSize)
+    def recv_data(connection):
+        """
+        Receives bytes from a connection. First, its reads how many bytes were sent.
+        :param connection: the connection to be used to receive the object.
+        :return: the received data.
+        """
+        data_size = struct.unpack("!i", connection.recv(4))[0]
+        return connection.recv(data_size)
 
     @staticmethod
-    def recvObject(connection):
-        return pickle.loads(NetUtils.recvData(connection))
+    def recv_object(connection):
+        """
+        Receives an object and de-serializes it.
+        :param connection: the connection to be used to receive the object.
+        :return: de-serialized object.
+        """
+        return pickle.loads(NetUtils.recv_data(connection))
 
     @staticmethod
-    def sendObject(connection, obj=None):
-        NetUtils.sendData(connection, pickle.dumps(obj))
+    def send_object(connection, obj=None):
+        """
+        Serialize the object with pickle and sends it through the connection.
+        :param connection: the connection that will be used to send the object.
+        :param obj: the object to be sent.
+        """
+        NetUtils.send_data(connection, pickle.dumps(obj))
+
+    @staticmethod
+    def checksum(data_str: str):
+        packet = data_str.strip().encode('utf-8')
+        total = 0
+        # Add up 16-bit words
+        num_words = len(packet) // 2
+        for chunk in struct.unpack("!%sH" % num_words, packet[0:num_words * 2]):
+            total += chunk
+
+        # Add any left over byte
+        if len(packet) % 2:
+            total += ord(chr(packet[-1])) << 8
+
+        # Fold 32-bits into 16-bits
+        total = (total >> 16) + (total & 0xffff)
+        total += total >> 16
+        return ~total + 0x10000 & 0xffff
+
+    @staticmethod
+    def verify_msg(data: str):
+        """
+        This method must be updated as the data format sent by the bbb is modified
+        BBB sends this: {chk} | {cmd} | {name} | {type} | {ipAddr} | {sha}
+        :param data: received packet to be verified.
+        :return: Array containing the data, [] if error.
+        """
+        splt = data.split("|")
+        if len(splt) != 6:
+            # Error !
+            return []
+        message = data[(len(splt[0]) + 1):]
+        res = NetUtils.checksum(message)
+        if int(res) == int(splt[0]):
+            return splt
+        else:
+            return []
+
+    re_ipv4 = r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"
+    re_mask_ipv4 = r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"
+
+    @staticmethod
+    def get_ip_address(if_name: str):
+        if_name = if_name.encode('utf-8')
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', if_name[:15])
+        )[20:24])
+
+    @staticmethod
+    def change_ip(desired_ip: str = None, interface_name: str = 'eth0', net_mask: str = '255.255.255.0',
+                  gateway: str = None):
+        """
+        @todo: now that i know the service i can do whatever i want !
+        Execute the connmanclt tool to change the host' IP address
+        :param desired_ip: the new IP address
+        :param interface_name: ethernet interface to have its address changed.
+        :param net_mask: network of the new ip address
+        :param gateway: new default gateway
+        :return: (True or False), message
+        """
+        if not desired_ip:
+            return False, 'Desired ip not set !'
+
+        service = get_service(interface_name=interface_name)
+        if not service:
+            return False, 'Service could\'t be found'
+
+        # @todo make the magic happen
+        # @todo make two different patterns for mask and ip. Limit them with the correct values.
+        if not re.match(re_mask_ipv4, net_mask):
+            return False, 'The net_mask={} doesn\'t match the mask pattern.'.format(net_mask)
+        if not re.match(re_ipv4, desired_ip):
+            return False, 'The desiredIp={} doesn\'t match the ipv4 pattern.'.format(desired_ip)
+        if gateway:
+            if not re.match(re_ipv4, gateway):
+                return False, 'The gateway={} doesn\'t match the ipv4 pattern.'.format(gateway)
+        else:
+            g_aux = desired_ip.split('.')
+            if len(g_aux) == 4:
+                g_aux[3] = '1'
+                gateway = ''
+                for g in g_aux:
+                    gateway += g + '.'
+                gateway = gateway[:-1]
+
+        res = subprocess.check_output(
+            ['connmanctl config {} ipv4 manual {} {} {}'.format(service, desired_ip, net_mask, gateway)],
+            shell=True)
+        return True, 'Successfully modified the ipv4 address. {}'.format(res)
+
+    @staticmethod
+    def get_service(interface_name: str = 'eth0'):
+        """
+        Return the service name from an interface.
+        @fixme: services with spaces on their names won't be detected !
+        :return: The service name or None !
+        """
+        res = subprocess.check_output(['connmanctl services'], stderr=subprocess.STDOUT, shell=True).decode('utf-8')
+        res = res.split()
+        for s in res:
+            if s.startswith('ethernet_'):
+                res = subprocess.check_output(['connmanctl services --properties ' + s], stderr=subprocess.STDOUT,
+                                              shell=True).decode(
+                    'utf-8')
+                print(res)
+                res = res.split('\n')
+                for p in res:
+                    if p.strip().startswith('Ethernet'):
+                        print('Found it!' + p)
+                        data = p.split('[')[1].strip()[:-1].split(',')
+                        for d_info in data:
+                            d_info = d_info.strip()
+                            if d_info.startswith('Interface'):
+                                print(d_info)
+                                if d_info == 'Interface={}'.format(interface_name):
+                                    print('The service related to {} is {}'.format(interface_name, s))
+                                    return s
+                        print(data)
+        return None
