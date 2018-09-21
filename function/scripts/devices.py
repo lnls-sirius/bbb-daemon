c06a802d431b76ac4a4a80b3ae869ea00a673945
carneirofc <claudiofcarneiro@hotmail.com>
2018-09-21 20:57:59 -0300

    Socat renamed to Functions

---
+++ function/scripts/devices.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+from os import environ, path
+from serial import Serial, STOPBITS_TWO, SEVENBITS, PARITY_EVEN
+
+import Adafruit_BBIO.GPIO as GPIO
+from PRUserial485 import PRUserial485_address
+
+from persist import persist_info
+from consts import *
+
+def counter_pru():
+    """
+    Counter PRU
+    """
+    if PRUserial485_address() != 21 and not path.isfile(PORT):
+        persist_info('PRU_CONTADORA', 115200, PRU_CONTADORA )
+
+def no_tty():
+    """
+    NO /dev/tttyUSB0
+    """
+    if not path.exists(PORT) and PRUserial485_address() == 21:
+        persist_info('NO_TTY_USB0', 115200, NOTTY )
+
+def power_suppply_pru():
+    """
+    PRU Power Supply 
+    """
+    GPIO.setup("P8_11", GPIO.IN)
+    GPIO.setup("P8_12", GPIO.IN)
+
+    if GPIO.input("P8_11") == 1 and GPIO.input("P8_12") == 1:
+        persist_info('PRU_FONTES', 115200, PRU_FONTES )
+
+
+
+def thermoIncluirChecksum(entrada):
+    soma = 0
+    for elemento in entrada:
+        soma += ord(elemento)
+    soma = soma % 256
+    return(entrada + "{0:02X}".format(soma) + "\x03")
+
+def thermo_probe():
+    """
+    Thermo probes
+    """
+    baud = 19200
+    ser = Serial(port = PORT,
+                baudrate = baud,
+                bytesize = SEVENBITS,
+                parity = PARITY_EVEN,
+                stopbits = STOPBITS_TWO,
+                timeout = TIMEOUT)
+    msg = thermoIncluirChecksum("\x07" + "01RM1")
+    ser.write(msg.encode('utf-8'))
+    res = ser.read(50)
+
+    if len(res) != 0:
+        persist_info('Thermo_Probe', baud, SERIAL_THERMO)
+
+def mbtempChecksum(entrada):
+    print('{}'.format(entrada))
+    soma = 0
+    for elemento in entrada:
+        soma += ord(elemento)
+    soma = 256 - soma % 256
+    return "0x%x"%soma
+ 
+def mbtemp():
+    """
+    MBTemp
+    """
+    baud = 115200
+    ser = Serial(PORT, baud, timeout=TIMEOUT)
+    try:
+        mbt_addr = 0
+        for msg in MBTEMP_CONSTS:
+            mbt_addr += 1
+            ser.write(msg) 
+            res = ser.read()
+            if len(res) != 0: 
+                persist_info('mbtemp', baud, MBTEMP, 'MBTemp Addr {}'.format(mbt_addr))
+    except Exception as e:
+        print('{}'.format(e))
+    ser.close()
+
+def mks9376b():
+    baud = 115200
+    ser = Serial(port=PORT, baudrate=baud, timeout=TIMEOUT)
+    for i in range (1, 254):
+        try:
+            msgm = '\@{0:03d}'.format(i) + "PR1?;FF"
+            ser.write(msgm)
+            res = ser.read()
+            if len(res) != 0:  
+                persist_info('mks9637b', baud, MKS937B, 'First MKS937b at addr {}'.format(i))
+        except Exception as e:
+            print('{}'.format(e))
+    ser.close()
+
+def agilent4uhv():
+    """
+    AGILENT 4UHV
+    """
+    pass
+
+def spixcon():
+    """
+    SPIxCON
+    """
+    pass
---
+++ function/scripts/functions.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+function overlay_PRUserial485 {
+    echo Initializing PRUserial485 overlay.
+
+    if [ ! -d /root/pru-serial485 ]; then
+        echo ERROR! The folder /root/pru-serial485 doesn\'t exist.
+        exit 1
+    fi
+
+    if [ ! -f /root/pru-serial485/src/overlay.sh ]; then
+        echo ERROR! The file /root/pru-serial485/src/overlay.sh doesn\'t exist.
+        exit 1
+    fi 
+
+    pushd /root/pru-serial485/src
+    ./overlay.sh
+    popd 
+}
+
+function overlay_SPIxCONV {
+    echo Initializing SPIxCONV overlay.
+
+    if [ ! -d /root/SPIxCONV ]; then
+        echo ERROR! The folder /root/SPIxCONV doesn\'t exist.
+        exit 1
+    fi
+
+    if [ ! -f /root/SPIxCONV/init/SPIxCONV_config-pin.sh ]; then
+        echo ERROR! The file /root/SPIxCONV/init/SPIxCONV_config-pin.sh doesn\'t exist.
+        exit 1
+    fi 
+
+    pushd /root/SPIxCONV/init
+    ./SPIxCONV_config-pin.sh
+    popd 
+}
+
+function pru_contadora {
+    echo Socat not started. No ttyUSB0 detected and PRUserial485_address isn\'t 21.
+    echo Initializing Counter PRU ...
+
+    if [ ! -d /root/counting-pru ]; then
+        echo ERROR! The folder /root/counting-pru doesn\'t exist.
+        exit 1
+    fi
+
+    if [ ! -f /root/counting-pru/IOC/v2-2/SI-CountingPRU_Socket.py ]; then
+        echo ERROR! The file /root/counting-pru/IOC/v2-2/SI-CountingPRU_Socket.py doesn\'t exist.
+        exit 1
+    fi 
+
+    pushd /root/counting-pru/IOC/v2-2
+    ./SI-CountingPRU_Socket.py
+    echo SI-CountingPRU_Socket.py Terminated !
+    popd
+}
