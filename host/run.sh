d63e980b0b93f34c026527c63267be5afb02f6d5
Patricia Nallin <patricia.nallin@gmail.com>
2018-10-18 11:56:22 -0300

    Updating files via rsync

--- host/run.sh
+++ host/run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 export SERVER_APP_IP="10.0.6.44"
 
+'''
 pushd ${PWD}/../
     git pull
+popd
+'''
+
+# Using rsync service
+pushd rsync
+    echo Synchronizing bbb-daemon files
+    ./rsync_beaglebone.sh bbb-daemon
 popd
 
 python3 run_daemon.py
