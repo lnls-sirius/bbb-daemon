8d69ae77afbf10bc83ffcf8fb7b68229527ac91a
carneirofc <claudiofcarneiro@hotmail.com>
2018-10-30 15:58:00 -0300

    Host: run-daemon wrong env.

--- host/run/run-daemon.sh
+++ host/run/run-daemon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-pushd $DAEMON_BASE/host
+export DAEMON_BASE=/root/bbb-daemon
 
-    export DAEMON_BASE=/root/bbb-daemon
+pushd $DAEMON_BASE/host
     export PYTHONPATH=${DAEMON_BASE}
-    export CONFIG_PATH=$DAEMON_BASE"/bbb.bin"
+    export CONFIG_PATH=${DAEMON_BASE}/bbb.bin
     export SERVER_ADDR="10.0.6.44"
     export BIND_PORT=9877
     export PING_PORT=9876
