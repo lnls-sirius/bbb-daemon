54a15a0a7efa437efdfc5a68d572e037f8f2dce2
carneirofc <claudiofcarneiro@hotmail.com>
2018-10-30 15:23:56 -0300

    Host: Merge

--- host/function/scripts/whoami.py
+++ host/function/scripts/whoami.py
@@ -3,13 +3,7 @@ import logging
 import time
 
 from serial import Serial
-<<<<<<< HEAD
 from os import environ, path
-=======
-from os import environ
-from os import path
-#!/usr/bin/python
->>>>>>> 2fd49ea74b703db0ef4e4cb3e8dc96092c1883d2
 from persist import persist_info
 
 
@@ -23,17 +17,10 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%d/%m/%Y %H:%M:%S')
 
 logger = logging.getLogger('Whoami')
-<<<<<<< HEAD
 logger.info('Iterating through possible devices ...')
 
 # Loop until detect something
 while not path.isfile(RES_FILE) or not path.isfile(BAUDRATE_FILE):
-=======
-
-while path.:
-    pass
-    logger.info('Iterating through possible devices ...')
->>>>>>> 2fd49ea74b703db0ef4e4cb3e8dc96092c1883d2
     try:
         no_tty()
         counting_pru()
@@ -48,9 +35,6 @@ while path.:
     except:
         logger.exception('Something wrong happened !')
 
-<<<<<<< HEAD
     time.sleep(2.)
-=======
->>>>>>> 2fd49ea74b703db0ef4e4cb3e8dc96092c1883d2
 
 logger.info('End of the identification Script ...')
