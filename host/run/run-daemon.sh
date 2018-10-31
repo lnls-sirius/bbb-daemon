8a1314c0b6f006cecaee7175f989f3535136847a
carneirofc <claudiofcarneiro@hotmail.com>
2018-10-31 12:35:42 -0300

    Host: Set the server ip as 10.0.6.48.

--- host/run/run-daemon.sh
+++ host/run/run-daemon.sh
@@ -4,7 +4,7 @@ export DAEMON_BASE=/root/bbb-daemon
 pushd $DAEMON_BASE/host
     export PYTHONPATH=${DAEMON_BASE}
     export CONFIG_PATH=${DAEMON_BASE}/bbb.bin
-    export SERVER_ADDR="10.0.6.44"
+    export SERVER_ADDR="10.0.6.48"
     export BIND_PORT=9877
     export PING_PORT=9876
     export PING_CANDIDATES="10.0.6.44 10.0.6.48 10.0.6.51"
