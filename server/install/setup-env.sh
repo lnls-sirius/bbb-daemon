12a6cdc4ac78c5017b23b847ea934391d173cdbd
carneirofc <claudiofcarneiro@hotmail.com>
2018-12-10 06:38:42 -0200

    Install config folder.

---
+++ server/install/setup-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pushd ../bbb-daemon-virtualenv/bin
+    source activate
+popd
+
+echo "Using Python 3 on  bbb-daemon-virtualenv ..."
+echo "Installing requirements ..."
+pip3 install -r ../../docker/bbb-server/requirements.txt
+pip3 install -I gunicorn==19.9.0
+pip3 install Flask-Cors==3.0.7
+deactivate
