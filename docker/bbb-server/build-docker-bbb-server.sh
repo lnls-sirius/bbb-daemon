6632f5eb7d1d4e0352f017e199e3237a1284b958
carneirofc <claudiofcarneiro@hotmail.com>
2018-12-11 07:45:02 -0200

    Update docker related files.

--- docker/bbb-server/build-docker-bbb-server.sh
+++ docker/bbb-server/build-docker-bbb-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DOCKER_MANTAINER_NAME=lnlscon
-DOCKER_NAME=bbb-daemon-container
+DOCKER_NAME=cons-bbb-daemon
 DOCKER_TAG=latest
 
 docker build -t ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG} .
--- docker/bbb-server/run-redis.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -v /storage/bbb-daemon:/data -p 6379:6379 --name bbb-redis -d redis redis-server --appendonly yes
