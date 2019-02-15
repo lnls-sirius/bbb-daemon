68b97a593d11866245319e432c184dfb4ec038a6
carneirofc <claudiofcarneiro@hotmail.com>
2019-02-15 11:13:58 -0200

    Utility function.

---
+++ common/util/config_table.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../'))
+from common.entity.entities import Type
+
+for i in range(1,22):
+    if i == 21:
+        # Fontes das  LT
+        print(u'10.128.{0}.101 10.128.{0}.0/24 {1}'.format(i+100,Type(code=Type.POWER_SUPPLY).code))
+        print(u'10.128.{0}.102 10.128.{0}.0/24 {1}'.format(i+100,Type(code=Type.POWER_SUPPLY).code))
+        print(u'10.128.{0}.103 10.128.{0}.0/24 {1}'.format(i+100,Type(code=Type.POWER_SUPPLY).code))
+        print(u'10.128.{0}.104 10.128.{0}.0/24 {1}'.format(i+100,Type(code=Type.POWER_SUPPLY).code))
+    else:
+        print(u'10.128.{0}.101 10.128.{0}.0/24 {1}'.format(i+100,Type(code=Type.MKS937B).code))
+        print(u'10.128.{0}.102 10.128.{0}.0/24 {1}'.format(i+100,Type(code=Type.AGILENT4UHV).code))
+        print(u'10.128.{0}.103 10.128.{0}.0/24 {1}'.format(i+100,Type(code=Type.AGILENT4UHV).code))
+        print(u'10.128.{0}.104 10.128.{0}.0/24 {1}'.format(i+100,Type(code=Type.POWER_SUPPLY).code))
+        print(u'10.128.{0}.105 10.128.{0}.0/24 {1}'.format(i+100,Type(code=Type.POWER_SUPPLY).code))
+        print(u'10.128.{0}.106 10.128.{0}.0/24 {1}'.format(i+100,Type(code=Type.MBTEMP).code))
+
+    if i == 1:
+        # EPP
+        print(u'10.128.101.107 10.128.101.0/24 {}'.format(Type.SPIXCONV))
+        print(u'10.128.101.108 10.128.101.0/24 {}'.format(Type.SPIXCONV))
+        print(u'10.128.101.109 10.128.101.0/24 {}'.format(Type.SPIXCONV))
+        print(u'10.128.101.110 10.128.101.0/24 {}'.format(Type.SPIXCONV))
+        print(u'10.128.101.111 10.128.101.0/24 {}'.format(Type.SPIXCONV))
+        print(u'10.128.101.112 10.128.101.0/24 {}'.format(Type.SPIXCONV))
+        print(u'10.128.101.113 10.128.101.0/24 {}'.format(Type.SPIXCONV))
+    if i == 20:
+        # EPP
+        print(u'10.128.120.107 10.128.120.0/24 {}'.format(Type.SPIXCONV))
+        print(u'10.128.120.108 10.128.120.0/24 {}'.format(Type.SPIXCONV))
+        print(u'10.128.120.109 10.128.120.0/24 {}'.format(Type.SPIXCONV))
+        print(u'10.128.120.110 10.128.120.0/24 {}'.format(Type.SPIXCONV))
