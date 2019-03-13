0194df3714e5e0f2a89be2437598972b1eeabe05
carneirofc <claudiofcarneiro@hotmail.com>
2019-03-13 12:17:29 -0300

    bbb-daemon: chmod to file.

--- host/function/scripts/functions.sh
+++ host/function/scripts/functions.sh
@@ -73,7 +73,8 @@ function overlay_SPIxCONV {
     fi
 
     pushd /root/SPIxCONV/init
-    ./SPIxCONV_config-pin.sh
+        chmod +x SPIxCONV_config-pin.sh
+       ./SPIxCONV_config-pin.sh
     popd
 }
 
