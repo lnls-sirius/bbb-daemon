4ca78576b21bf6392ad619dbf09051897a7424b7
ximenes <xresende@gmail.com>
2019-04-12 18:32:08 -0300

    Update server of constants

--- host/function/scripts/ps-ioc-config-files/sync-fac-files.sh
+++ host/function/scripts/ps-ioc-config-files/sync-fac-files.sh
@@ -11,7 +11,6 @@ pushd /root/bbb-daemon/host/rsync
     sed -i -e '/127.0.0.1/d' -e '/127.0.1.1/d' /etc/hosts
     # Writing new values, described below
     sed -i -e '1i\127.0.0.1    localhost' -e '1i\127.0.1.1    '"$hn" /etc/hosts
-    sed -i -e '$a\'"#"' sirius-consts server alias' -e '$a\10.128.254.203 sirius-consts.lnls.br' /etc/hosts
-    
-popd
+    sed -i -e '$a\'"#"' sirius-consts server alias' -e '$a\10.128.255.5 sirius-consts.lnls.br servweb' /etc/hosts
 
+popd
