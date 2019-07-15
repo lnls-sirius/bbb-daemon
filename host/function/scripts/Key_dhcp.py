233a83ceea2cde790606cf1ac731b4a85e3c9256
Robert Polli <robert.polli@lnls.br>
2019-07-15 16:14:33 -0300

    Atualizacao log

--- host/function/scripts/Key_dhcp.py
+++ host/function/scripts/Key_dhcp.py
@@ -6,6 +6,8 @@ import commands
 import Adafruit_BBIO.GPIO as GPIO
 from time import sleep
 
+os.system("echo 'Verificando condição DHCP em Hardware'")
+
 def dhcp():		#Set IP to DHCP
      service = commands.getoutput("(connmanctl services |awk '{print $3}')")
      os.system("connmanctl config %s --ipv4 dhcp" %(service))
@@ -24,7 +26,8 @@ GPIO.setup("P8_11", GPIO.IN)		#Pin out configurations
 GPIO.setup("P8_12", GPIO.IN)
 GPIO.setup("P8_28", GPIO.OUT)
 GPIO.output("P8_28", GPIO.LOW)
-									                                 #   |on|     		
+									                                 #   |on|
 if (GPIO.input("P8_11") == 1 and GPIO.input("P8_12") == 0): #Check if the keys are set to the DHCP position  |dP|
+    os.system("echo 'Configurando DHCP'")
      dhcp()								                                 #   |12|
      led()
