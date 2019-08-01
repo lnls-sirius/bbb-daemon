9765304cacaecac3d50a68104305f3f283e85f87
ximenes <xresende@gmail.com>
2019-08-01 11:59:11 -0300

    Enable FAC IOC service running back at boot time

--- host/function/scripts/ps-ioc-config-files/run-fac-ps-ioc.sh
+++ host/function/scripts/ps-ioc-config-files/run-fac-ps-ioc.sh
@@ -3,8 +3,8 @@
 
 
 # Wait until servweb is up and running
-#wait-for-it -t 0 10.128.255.5:80
-#wait-for-it -t 0 10.128.255.5:20080
+wait-for-it -t 0 10.128.255.5:80
+wait-for-it -t 0 10.128.255.5:20080
 
 # Run PS IOC systemd service
-#systemctl start sirius-bbb-ioc-ps.service
+systemctl start sirius-bbb-ioc-ps.service
